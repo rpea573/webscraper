10175243eaeb2b6bf09aebe4f1adb8b588fbbc23
Andrew <r.peagram92@gmail.com>
2021-12-18 23:15:46 +1300

    Restarted the scraper, using a basic frame from a stackoverflow post and added the logic in. Successfully scrapes oneroof as intended.

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
+const { last } = require("cheerio/lib/api/traversing");
 const puppeteer = require("puppeteer");
-const fs = require("fs/promises");
-const { appendFileSync } = require("fs");
-const { title } = require("process");
 
-// Function to retrieve the featured listing's data on Oneroof's sale list
+let scrape = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.oneroof.co.nz/search/sold/suburb_mount-albert-auckland-city-960,sandringham-auckland-city-2212,waterview-auckland-city-2155_bedroom_5_page_1"
+  );
+
+  let lastPageNumber = await page.evaluate(() => {
+    return document.querySelector(".pager").lastChild.innerText;
+  });
+
+  lastPageNumber = parseInt(lastPageNumber);
+
+  let results = []; // variable to hold collection of all book titles and prices
+  for (let i = 0; i < lastPageNumber; i++) {
+    // wait 1 sec for page load
+    await page.waitForTimeout(1000);
+    // call and wait extractedEvaluateCall and concatenate results every iteration.
+    // You can use results.push, but will get collection of collections at the end of iteration
+    results = results.concat(await pageListings(page));
+    console.log("inside");
+    // this is where next button on page clicked to jump to another page
+    if (i != lastPageNumber) {
+      // no next button on last page
+      await page.click(".btn-next");
+    }
+  }
+  console.log("outside");
+  //   browser.close();
+  return results;
+};
+
 const retrieveFeatured = () => {
   return Array.from(document.querySelectorAll(".house-feature"), (feature) => {
     let bed = "";
@@ -42,7 +71,6 @@ const retrieveFeatured = () => {
   });
 };
 
-//Retrieve the standard listing's data on Oneroof's sold list
 const retrieveStandard = () => {
   return Array.from(
     document.querySelectorAll(".house-standard .info"),
@@ -84,57 +112,17 @@ const retrieveStandard = () => {
   );
 };
 
-let salesData = [];
+async function pageListings(page) {
+  let listings = [];
+  let featured = await page.evaluate(retrieveFeatured);
+  let standard = await page.evaluate(retrieveStandard);
 
-// Controller - we will to go to next page and repeat this and continuously add the new data to the end of an array. We will then need to save the data...
-const scrape = async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  // const url =
-  //   "https://www.oneroof.co.nz/search/sold/suburb_avondale-auckland-city-153,blockhouse-bay-auckland-city-2776,kingsland-auckland-city-843,mount-albert-auckland-city-960,mount-roskill-auckland-city-589,new-windsor-auckland-city-1491,onehunga-auckland-city-2899,sandringham-auckland-city-2212,waterview-auckland-city-2155_page_1";
+  return (listings = listings.concat(featured, standard));
+}
 
-  const url =
-    "https://www.oneroof.co.nz/search/sold/suburb_mount-albert-auckland-city-960,sandringham-auckland-city-2212,waterview-auckland-city-2155_bedroom_5_page_1";
-
-  await page.goto(url);
-
-  let nextPage = true;
-  let count = 1;
-  while (nextPage) {
-    let featured = await page.evaluate(retrieveFeatured);
-    let standard = await page.evaluate(retrieveStandard);
-
-    if (featured.length > 0) {
-      salesData = salesData.concat(featured);
-    }
-
-    if (standard.length > 0) {
-      salesData = salesData.concat(standard);
-    }
-
-    console.log(count);
-    count += 1;
-
-    nextPage = await page.evaluate(() => {
-      let nextButton = document.querySelector(".btn-next").className;
-      if (nextButton.includes("notap")) {
-        return false;
-      } else {
-        return true;
-      }
-    });
-
-    if (nextPage) {
-      await page.waitForNavigation({ waitUntil: "networkidle2" });
-      await page.click(".btn-next");
-    }
-
-    console.log(nextPage);
-  }
-
-  console.log(salesData);
-
-  await browser.close();
-};
-
-scrape();
+scrape().then((value) => {
+  console.log(value);
+  console.log("Collection length: " + value.length);
+  console.log(value[0]);
+  console.log(value[value.length - 1]);
+});
